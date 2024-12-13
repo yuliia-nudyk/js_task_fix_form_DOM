7e1b621cac02fd835918c19a60e2515f500e7e09
Yuliia Nudyk <yulyayu833@gmail.com>
2024-12-13 20:50:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const formInputs = document.querySelectorAll('input');
+
+const getSeparatedStr = (str) => {
+  const upperCaseLetters = [];
+
+  for (const char of str) {
+    if (char === char.toUpperCase()) {
+      upperCaseLetters.push(char);
+    }
+  }
+
+  if (!upperCaseLetters.length) {
+    return str;
+  }
+
+  let newStr = str;
+
+  upperCaseLetters.forEach((letter) => {
+    newStr = newStr.split(letter).join(` ${letter}`);
+  });
+
+  return newStr;
+};
+
+formInputs.forEach((input) => {
+  const nameOfInput = getSeparatedStr(input.name);
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+  label.setAttribute('htmlFor', input.name);
+  label.textContent = nameOfInput;
+
+  input.parentElement?.append(label);
+
+  input.placeholder = nameOfInput[0].toUpperCase() + nameOfInput.slice(1);
+});
